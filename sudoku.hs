bca670bbaeb16d9a2f11e73367237ab7eb086adb
Simon Kröger <simon@quad.(none)>
2011-10-18 23:16:18 +0200

    faster, shorter, better ..

--- sudoku.hs
+++ sudoku.hs
@@ -1,49 +1,39 @@
 import Data.Char
 import Data.List
+import Data.Array
+import Data.Maybe
 
 {-
-neighbourhood i = 
-  filter (sameNeighbourhood i) [0..80]
-  where
-  sameNeighbourhood i j =
-    row i == row j || col i == col j || box i == box j
-    where row x = x `div` 9
-          col x = x `mod` 9
-          box x = x `div` 27 * 3 + x `mod` 9 `div` 3
+neighbourhood i = filter (neighbours i) [0..80]
+  where neighbours i j = box i == box j || row i == row j || col i == col j
+        box n = n `div` 27 * 3 + n `mod` 9 `div` 3
+        row n = n `div` 9
+        col n = n `mod` 9
 -}
 
--- can't believe there are no build in versions of this
-valuesAt :: (Integral t) => [a] -> [t] -> [a]
-valuesAt _ [] = [] 
-valuesAt xs (y:ys) = (genericIndex xs y):valuesAt xs ys
-
-replaceAt :: (Integral t) => [a] -> t -> a -> [a]
-replaceAt xs n a = (genericTake n xs) ++ [a] ++ (genericDrop (n+1) xs)
-
+-- can't believe there is no build in version of this
 groupsOf :: (Integral t) => t -> [a] -> [[a]] 
 groupsOf _ [] = []
 groupsOf n xs = genericTake n xs : groupsOf n (genericDrop n xs)  
-
---
 
 row = [[x*9..x*9+8] | x <- [0..8]]
 col = [[x,x+9..x+72] | x <- [0..8]]
 box = [map (y*27+x*3+) [0, 1, 2, 9, 10, 11, 18, 19, 20] | x <- [0..2], y <- [0..2]]
 
 solveGame g index try
-  | index > 80         = g
-  | try > 9            = []
-  | g !! index /= 0    = solveGame g (index+1) 1
+  | index > 80         = Just g
+  | try > 9            = Nothing
+  | g ! index /= 0     = solveGame g (index+1) 1
   | not (validGame g') = solveGame g index (try+1)
-  | solved == []       = solveGame g index (try+1)
+  | isNothing solved   = solveGame g index (try+1)
   | otherwise          = solved
-  where g' = replaceAt g index try
+  where g' = g // [(index, try)]
         solved = solveGame g' (index+1) 1
 
-validGame :: [Int] -> Bool
+validGame :: Array Int Int -> Bool
 validGame g = all valid (row ++ col ++ box)
   where valid = all (==1) . map length . groups
-        groups = group . sort . filter (>0) . valuesAt g
+        groups = group . sort . filter (>0) . map (g!)
 
 parseGame :: String -> [Int]
 parseGame = foldr toNumber []
@@ -59,8 +49,9 @@ readGame xs = do
 
 main = do
   s <- readGame ""
-  let solved = solveGame (parseGame s) 0 1
+  let game = listArray (0, 80) $ parseGame s
+  let solved = elems . fromJust $ solveGame game 0 1
   let lines = groupsOf 18 (intersperse ' ' (map intToDigit solved))
 
-  putStrLn (intercalate "\n" lines)
+  putStrLn $ unlines lines
   return ()
